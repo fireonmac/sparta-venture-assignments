5965e13949378f4e3a57983756609bd95f94cc58
Noh Hyunho <fireonmac@gmail.com>
2025-10-09 04:04:09 +0900

    add more comments for StudentScoreManager and remove unused student util file

--- dart_advanced/lib/student_score_manager.dart
+++ dart_advanced/lib/student_score_manager.dart
@@ -18,12 +18,15 @@ class StudentScoreManager {
     return total / _scores.length;
   }
 
+  /// 학생 정보가 기록된 파일을 불러와서 `List<StudentScore>` 형태로 [_scores]에 저장합니다
+  /// 기존 데이터가 있는 경우 삭제합니다
   void load([String filePath = 'students.txt']) {
     var data = FileUtil.loadFile(filePath);
     _scores.clear();
     _scores.addAll(_parse(data));
   }
 
+  /// [StudentScore] 정보를 파일에 저장합니다
   void save(StudentScore score, [String filePath = 'result.txt']) {
     FileUtil.saveFile(filePath, score.toString());
   }
@@ -44,7 +47,7 @@ class StudentScoreManager {
   /// 사용자 입력을 받아 `메뉴 방식`으로 기능을 선택할 수 있도록 합니다
   /// 사용자가 `1`을 입력하면, 우수생(가장 높은 평균 점수를 가진 학생)의 정보를 출력합니다
   /// 사용자가 `2`를 입력하면, 전체 학생들의 평균 점수를 계산하여 출력합니다
-  /// 사용자가 `3`을 입력하여 프로그램을 종료하기 전까지 반복해서 재실행합니다
+  /// 사용자가 `3` 또는 잘못된 입력 값을 넣는 경우 프로그램을 종료하며, 그 전까지는 반복 실행합니다
   void promptMenu() {
     print('''
 메뉴를 선택하세요:
@@ -78,7 +81,7 @@ class StudentScoreManager {
     }
   }
 
-  /// '<이름>,<점수>' 형식의 문자열 데이터를 `List<StudentScore>`로 변환합니다.
+  /// '<이름>,<점수>' 형식의 문자열 데이터를 `List<StudentScore>`로 변환합니다
   ///
   /// 입력 데이터 예시:
   /// ```
@@ -86,20 +89,20 @@ class StudentScoreManager {
   /// 김철수,80
   /// ```
   ///
-  /// 입력 데이터에서 파싱된 [StudentScore] 객체들의 리스트를 반환합니다.
+  /// 입력 데이터에서 파싱된 [StudentScore] 객체들의 리스트를 반환합니다
   ///
-  /// 형식이 맞지 않는 줄이 있으면 [FormatException]을 발생시킵니다.
+  /// 형식이 맞지 않는 줄이 있으면 [FormatException]을 발생시킵니다
   List<StudentScore> _parse(String data) {
     return data.split('\n').map((line) {
       var parts = line.split(',');
-      
+
       if (parts.length != 2) {
         throw FormatException('잘못된 데이터 형식: $line');
       }
 
       var name = parts[0];
       var point = int.tryParse(parts[1]);
-      
+
       if (point == null) {
         throw FormatException('점수가 숫자가 아닙니다: $point');
       }
