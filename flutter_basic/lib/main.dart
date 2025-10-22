5e6b99e9eb8140ea856d914c95d270d88f4ea489
Noh Hyunho <fireonmac@gmail.com>
2025-10-22 15:34:13 +0900

    Tasks 플러터 프로젝트 생성

---
+++ flutter_basic/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tasks',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Scaffold(body: const Text('Tasks')),
+    );
+  }
+}
