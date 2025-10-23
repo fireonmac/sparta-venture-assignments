d4ca0e9a246350c73f2b0ecdb4a988168c9603f0
Noh Hyunho <fireonmac@gmail.com>
2025-10-23 11:33:14 +0900

    상세 페이지 UI 및 기능 구현

--- flutter_basic/lib/src/todo_detail_page.dart
+++ flutter_basic/lib/src/todo_detail_page.dart
@@ -1,10 +1,111 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basic/src/styles.dart';
+import 'package:flutter_basic/src/todos_provider.dart';
 
-class TodoDetailPage extends StatelessWidget {
-  const TodoDetailPage({super.key});
+class TodoDetailPage extends StatefulWidget {
+  final int index;
+  const TodoDetailPage({super.key, required this.index});
+
+  @override
+  State<TodoDetailPage> createState() => _TodoDetailPageState();
+}
+
+class _TodoDetailPageState extends State<TodoDetailPage> {
+  final _descriptionController = TextEditingController();
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (_descriptionController.text.isEmpty) {
+      final todosNotifier = TodosProvider.of(context);
+      final todo = todosNotifier.todos[widget.index];
+      _descriptionController.text = todo.description ?? '';
+    }
+  }
+
+  @override
+  void dispose() {
+    _descriptionController.dispose();
+    super.dispose();
+  }
+
+  void _updateDescription() {
+    final todosNotifier = TodosProvider.of(context);
+    todosNotifier.updateDescription(widget.index, _descriptionController.text);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Text('TodoDetailPage');
+    final todosNotifier = TodosProvider.of(context);
+
+    return ListenableBuilder(
+      listenable: todosNotifier,
+      builder: (context, child) {
+        final todo = todosNotifier.todos[widget.index];
+        return Scaffold(
+          appBar: AppBar(
+            actions: [
+              Padding(
+                padding: const EdgeInsets.only(right: 8),
+                child: IconButton(
+                  icon: Icon(todo.isFavorite ? Icons.star : Icons.star_border),
+                  onPressed: () {
+                    todosNotifier.toggleIsFavorite(widget.index);
+                  },
+                ),
+              ),
+            ],
+          ),
+          body: Padding(
+            padding: Styles.bodyPadding,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  todo.title,
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Icon(Icons.short_text_rounded),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(height: 2),
+                          Text('세부 내용은 다음과 같습니다.'),
+                          SizedBox(height: 30),
+                          TextField(
+                            controller: _descriptionController,
+                            maxLines: null,
+                            minLines: 3,
+                            textAlignVertical: TextAlignVertical.top,
+                            decoration: const InputDecoration(
+                              hintText: '세부 내용은 여기에 작성합니다.',
+                              border: InputBorder.none,
+                              alignLabelWithHint: true,
+                              contentPadding: EdgeInsets.zero,
+                            ),
+                            onChanged: (value) {
+                              _updateDescription();
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 }
--- flutter_basic/lib/src/todos_notifier.dart
+++ flutter_basic/lib/src/todos_notifier.dart
@@ -4,12 +4,21 @@ import 'package:flutter_basic/src/todo_entity.dart';
 
 class TodosNotifier with ChangeNotifier {
   final List<TodoEntity> _todos = [
-    TodoEntity(title: '안녕', isFavorite: false, isDone: false)
+    TodoEntity(
+      title: '과제 제출하기',
+      description: '10월 23일 정오까지 제출',
+      isFavorite: true,
+      isDone: true,
+    ),
   ];
 
   UnmodifiableListView<TodoEntity> get todos => UnmodifiableListView(_todos);
 
-  void add({required String title, String? description, bool isFavorite = false}) {
+  void add({
+    required String title,
+    String? description,
+    bool isFavorite = false,
+  }) {
     if (title.isNotEmpty) {
       _todos.add(
         TodoEntity(
@@ -20,7 +29,6 @@ class TodosNotifier with ChangeNotifier {
         ),
       );
     }
-
     notifyListeners();
   }
 
@@ -31,6 +39,11 @@ class TodosNotifier with ChangeNotifier {
 
   void toggleIsFavorite(int index) {
     _todos[index].isFavorite = !_todos[index].isFavorite;
+    notifyListeners();
+  }
+
+  void updateDescription(int index, String description) {
+    _todos[index].description = description.isEmpty ? null : description;
     notifyListeners();
   }
 
