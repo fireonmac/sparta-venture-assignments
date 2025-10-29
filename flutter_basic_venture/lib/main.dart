4a981bb09f3ada8a5cb51d8588abdb9bc8951994
Noh Hyunho <fireonmac@gmail.com>
2025-10-30 02:36:56 +0900

    파이어베이스 프로젝트 설정

--- flutter_basic_venture/lib/main.dart
+++ flutter_basic_venture/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_basic_venture/firebase_options.dart';
 import 'package:flutter_basic_venture/src/constants.dart';
 import 'package:flutter_basic_venture/src/styles.dart';
 import 'package:flutter_basic_venture/src/features/products/products_container.dart';
@@ -7,7 +9,11 @@ import 'package:flutter_basic_venture/src/features/cart/cart_container.dart';
 import 'package:flutter_basic_venture/src/features/cart/cart_notifier.dart';
 import 'package:flutter_basic_venture/src/view/home/home_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
